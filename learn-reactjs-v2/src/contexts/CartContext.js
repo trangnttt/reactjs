769b12bc0025e1fc9005c32503a0fb7c1c285f06
trangnttt <ngotrang447@gmail.com>
2021-06-02 15:41:16 +0700

    commit:

--- learn-reactjs-v2/src/contexts/CartContext.js
+++ learn-reactjs-v2/src/contexts/CartContext.js
@@ -1,9 +1,9 @@
+import { compact } from 'lodash';
 import React, { createContext, useState } from 'react'
 export const CartContext = createContext();
 
 const CartProvider = (props) => {
-  const [dataCart] = useState({total: 0, data: []});
-
+  const [dataCart, setDataCart] = useState({ total: 0, data: [] });
   const addCart = (item) => {
     const { id, email } = item;
     const itemData = { id: id, email: email, qty: 1 };
@@ -25,11 +25,49 @@ const CartProvider = (props) => {
       return prev + current.qty
     }, 0);
     dataCart.total = sum;
-
-    console.log('data Cart', dataCart)
   }
+
+  const increase = (data) => {
+    const objIndex = dataCart.data.findIndex(obj => obj.id == data.id)
+    const qtyUpdate = data.qty + 1;
+    dataCart.data[objIndex].qty = qtyUpdate;
+    const sum = dataCart.data.reduce(function (prev, current) {
+      return prev + current.qty
+    }, 0);
+    dataCart.total = sum;
+    const test = { total: sum, data: dataCart.data }
+    setDataCart(test);
+  }
+
+  const decrease = (data) => {
+    if (data.qty > 0) {
+      const objIndex = dataCart.data.findIndex(obj => obj.id == data.id)
+      const qtyUpdate = data.qty - 1;
+      dataCart.data[objIndex].qty = qtyUpdate;
+      const sum = dataCart.data.reduce(function (prev, current) {
+        return prev + current.qty
+      }, 0);
+      dataCart.total = sum;
+      const datas = { total: sum, data: dataCart.data }
+      setDataCart(datas);
+    }
+  }
+
+  const erase = (data) => {
+    var index = dataCart.data.findIndex(obj => obj.id == data.id)
+    if (index > -1) {
+      dataCart.data.splice(index, 1);
+    }
+    const sum = dataCart.data.reduce(function (prev, current) {
+      return prev + current.qty
+    }, 0);
+    dataCart.total = sum;
+    const datas = { total: sum, data: dataCart.data }
+    setDataCart(datas);
+  }
+
   return (
-    <CartContext.Provider value={{ addCart, dataCart }}>
+    <CartContext.Provider value={{ addCart, increase, decrease, erase, dataCart }}>
       {props.children}
     </CartContext.Provider>
   )
--- learn-reactjs-v2/src/pages/Home/index.js
+++ learn-reactjs-v2/src/pages/Home/index.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Service from '@/components/Service'
 import Slides from '@/components/Slides'
-import ProductList  from '@/components/Product/ProductList'
+import ProductList from '@/components/Product/ProductList'
+import { CartContext } from '@/contexts/CartContext'
 
 function Home() {
+  const { dataCart, increase, decrease, erase } = useContext(CartContext);
+  console.log('dataCart home', dataCart)
   return (
     <div className="main">
       <Slides />
       <div className="container">
         <Service />
         <ProductList />
-
+        { }
         <h3>Cart</h3>
-
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Qty</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataCart.data.map((data, index) => (
+              <tr key={index}>
+                <td>{data.id}</td>
+                <td>{data.email}</td>
+                <td>
+                  <button onClick={() => decrease(data)}> - </button>
+                  <span> {data.qty} </span>
+                  <button onClick={() => increase(data)}> + </button>
+                </td>
+                <td><button onClick={() => erase(data)}>Delete</button></td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+          </tfoot>
+        </table>
       </div>
     </div>
   );
