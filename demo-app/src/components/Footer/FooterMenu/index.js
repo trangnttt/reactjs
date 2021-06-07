ff3a564f72a6edbe7ddb02fd0ef9681ec304ebe5
trangnttt <ngotrang447@gmail.com>
2021-06-07 09:18:56 +0700

    commit

--- demo-app/src/components/Footer/FooterMenu/index.js
+++ demo-app/src/components/Footer/FooterMenu/index.js
@@ -61,21 +61,19 @@ const FooterMenu = () => {
                     </div>
                     <div className="footer-menu__column">
                         <h3 className="footer-menu__title">About Us</h3>
-                        <ul className="menu-footer">
-                            <li>
-                                <a href="#">Menu 1</a>
-                            </li>
-                            <li>
-                                <a href="#">Menu 1</a>
-                            </li>
-
-                            <li>
-                                <a href="#">Menu 1</a>
-                            </li>
-                            <li>
-                                <a href="#">Menu 1</a>
-                            </li>
-                        </ul>
+                        <p className="footer-menu__sub">Stay up to date with news and promotions by signing up for our newsletter</p>
+                        <div className="footer-menu__search">
+                            <form action="#">
+                                <div className="group-form">
+                                    <input className="search-input" type="text" placeholder="Search.." name="search" />
+                                    <button className="search-btn" type="submit"><i className="fa fa-envelope-square"></i> Send</button>
+                                </div>
+                                <div className="group-form from-check">
+                                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                                    <label className="name-check">I have read and agree to the <a href="#">Privacy Policy</a></label>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
